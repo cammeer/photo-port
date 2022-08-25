7072b1d6350a6605c948132f69c15f9ea659b8c3
cardinal <caren@merzhaus.org>
2022-08-25 18:33:16 -0400

    sync with module code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react'
-import Nav from './components/Nav'
-import About from './components/About'
-import Gallery from './components/Gallery'
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
     {
       name: 'commercial',
-      description:
-        'Photos of grocery stores, food trucks, and other commercial projects',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
-    {
-      name: 'landscape',
-      description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
-    },
-  ])
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]); 
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0])
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
@@ -28,13 +24,11 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
